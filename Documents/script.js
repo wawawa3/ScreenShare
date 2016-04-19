a98ea649b74e8dff740a5002d34edb1f4580e733
Wawawa <id.253122@gmail.com>
2016-04-19 15:52:41 +0900

    Documents追加

---
+++ Documents/script.js
@@ -0,0 +1,526 @@
+function onLoad()
+{
+	var __DEBUG__ = true;
+
+	// utility ------------------------------------------------------------
+	if (!ArrayBuffer.prototype.slice)
+	{
+		ArrayBuffer.prototype.slice = function (start, end) {
+			var that = new Uint8Array(this);
+			if (end == undefined) end = that.length;
+			var result = new ArrayBuffer(end - start);
+			var resultArray = new Uint8Array(result);
+			for (var i = 0; i < resultArray.length; i++)
+			   resultArray[i] = that[i + start];
+			return result;
+		}
+	}
+
+	if (!AudioBufferSourceNode.prototype.start)
+		AudioBufferSourceNode.prototype.start = AudioBufferSourceNode.prototype.node;
+
+	// DOM ------------------------------------------------------------
+	var supportLabel =  document.getElementById("_support");
+	var audioCheckBox = document.getElementById("_audio");
+	var block = document.getElementById("_block");
+
+	// AppDef ------------------------------------------------------------
+	var ChankSize = 1024 * 32;
+	var FrameHeaderLength = 0x02;
+	var AudioHeaderLength = 0x04;
+
+	// server <-> client messages(WebSocket)
+	var MessageType = 
+	{
+		"Connected"				: 0x0,
+		"UpdateID"				: 0x1,
+		"PeerConnection"		: 0x2,
+		"SDPOffer"				: 0x3,
+		"SDPAnswer"				: 0x4,
+		"ICECandidateOffer"		: 0x5,
+		"ICECandidateAnswer"	: 0x6,
+		"RemoveOffer"			: 0x7,
+		"RemoveAnswer"			: 0x8,
+		"Settings"				: 0x9,
+		"StartCapture"			: 0xa,
+		"StopCapture"			: 0xb,
+		"ReceiveTimeout"		: 0xc,
+		"Disconnect"			: 0xd,
+	};
+
+	// server <-> client data type(WebRTC)
+	var DataType = 
+	{
+		"FrameBuffer" : 0x0,
+		"AudioBuffer" : 0x1,
+	};
+
+	// WebRTC ------------------------------------------------------------
+	var webRTCPeerConnection = (window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection || window.msRTCPeerConnection);
+	var webRTCSessionDescription = (window.RTCSessionDescription || window.mozRTCSessionDescription ||　window.webkitRTCSessionDescription || window.msRTCSessionDescription);
+	var webRTCIceCandidate = (window.RTCIceCandidate || window.mozRTCIceCandidate ||　window.webkitRTCIceCandidate || window.msRTCIceCandidate);
+
+	if (webRTCPeerConnection　&& webRTCSessionDescription && webRTCIceCandidate)
+	{
+		var pc_config = null;//{"iceServers":[]};
+
+		var peer_offer = [];
+		var peer_answer = null;
+
+		var dataChannel_offer = [];
+		var dataChannel_answer = null;
+
+		var peerID = null;
+		var peerConnectionCount = 0;
+		var directReceive = false;
+
+		var settings = null;
+
+		// FrameBuffer -------------------------------------------------------
+		var urlCreator = (window.URL || window.webkitURL);
+
+		var frameBuffer = new Uint8Array();
+		var intraImageData = [];
+
+		var canvasLocking = false;
+		var isCapturing = false;
+		
+		var divisionNum;
+		var loadingQueue;
+
+		var latestSegmentData = [];
+
+		var RTCReceiveTimeout = 1000;
+
+		// AudioBuffer -------------------------------------------------------
+		var audioContext = (window.AudioContext || window.webkitAudioContext);
+
+		if (audioContext)
+		{
+			var audioCtx = new audioContext();
+			var audioSamplerate = 4000;
+			var audioIsStereo = false;
+			var audioTime = 0;
+		}
+		else
+		{
+			supportLabel.style.visibility = "visible";
+			return;
+		}
+	}
+	else
+	{
+		supportLabel.style.visibility = "visible";
+		return;
+	}
+	
+
+	// WebSocket ------------------------------------------------------------
+	socket = new Alchemy(
+	{ 
+		Server: location.hostname,
+		Port: "8081",
+		BinaryType: "arraybuffer",
+		DebugMode: true
+	});
+
+	socket.Disconnected = function()
+	{
+		debugLog("disconnected");
+	};
+
+	socket.MessageReceived = function(event)
+	{
+		if (typeof event.data == "string")
+		{
+			var message = JSON.parse(event.data);
+
+			switch (message.type)
+			{
+				case MessageType.Connected:
+				debugLog("connected. my id: " + message.id);
+				peerID = message.id;
+				directReceive = (message.id == 0);
+				break;
+
+				case MessageType.UpdateID:
+				debugLog("update id. my id: " + message.id);
+				peerID = message.id;
+				directReceive = (message.id == 0);
+				break;
+
+				case MessageType.PeerConnection: // new peer connected
+				debugLog("new user connected. id: " + message.targetId);
+				peer_offer[message.targetId] = new webRTCPeerConnection(pc_config);
+				dataChannel_offer[message.targetId] = peer_offer[message.targetId].createDataChannel('dataChannel' + message.targetId);
+				dataChannel_offer[message.targetId].onopen = function(evt)
+				{
+					dataChannel_offer[message.targetId].binaryType = "arraybuffer";
+
+					if (latestSegmentData.length > 0)
+					{
+						debugLog("send buffer to new user");
+						for (var i = 0; i < latestSegmentData.length; i++)
+						{
+							var chank = segmentation(latestSegmentData[i], ChankSize);
+							sendData(dataChannel_offer[message.targetId], chank);
+						}
+					}
+				};
+
+				peer_offer[message.targetId].onicecandidate = function (evt)
+				{
+					if (evt.candidate)
+					{
+						var data = 
+						{
+							"type" : MessageType.ICECandidateOffer, 
+							"id": peerID, 
+							"targetId": message.targetId, 
+							"data": evt.candidate,
+						};
+						var json = JSON.stringify(data);
+						socket.Send(json);
+
+						debugLog("send ICECandidateOffer to id: " + message.targetId);
+					}
+					else
+						debugLog("icecandidate phase: " + evt.eventPhase);
+				};
+
+				peer_offer[message.targetId].createOffer(
+					function(sdp)
+					{
+						debugLog("createOffer succeeded.");
+
+						peer_offer[message.targetId].setLocalDescription(
+							sdp,
+							function()
+							{
+								var data = 
+								{
+									"type" : MessageType.SDPOffer, 
+									"id": peerID, 
+									"targetId": message.targetId, 
+									"data": sdp,
+								};
+								var json = JSON.stringify(data);
+								socket.Send(json);
+
+								debugLog("send SDPOffer to id: " + message.targetId);
+							},
+							function() { debugLog("setLocalDescription failed."); });
+					},
+					function() { debugLog("createOffer failed."); });
+
+				peerConnectionCount++;
+				debugLog("webRTC peer count: " + peerConnectionCount);
+				break;
+
+				case MessageType.SDPOffer: // SDP Offer Received
+				debugLog("received SDPOffer from id: " + message.id);
+				peer_answer = new webRTCPeerConnection(pc_config);
+				peer_answer.setRemoteDescription(
+					new webRTCSessionDescription(message.data),
+					function()
+					{
+						peer_answer.onicecandidate = function (evt)
+						{
+							debugLog("onicecandidate");
+
+							if (evt.candidate)
+							{
+								var data = 
+								{
+									"type" : MessageType.ICECandidateAnswer, 
+									"id": peerID, 
+									"targetId": message.id, 
+									"data": evt.candidate,
+								};
+								var json = JSON.stringify(data);
+								socket.Send(json);
+
+								debugLog("send ICECandidateAnswer to id: " + message.id);
+							}
+							else
+								debugLog("icecandidate phase: " + evt.eventPhase);
+						};
+						peer_answer.createAnswer(
+							function(sdp)
+							{
+								debugLog("createAnswer succeeded.");
+
+								peer_answer.setLocalDescription(
+									sdp, 
+									function()
+									{
+										debugLog("setLocalDescription succeeded.");
+
+										peer_answer.ondatachannel = function(evt)
+										{
+											var chank = [];
+
+											dataChannel_answer = evt.channel;
+											dataChannel_answer.binaryType = "arraybuffer";
+											dataChannel_answer.onmessage = function (event)
+											{
+												if (event.data != "\0")
+													chank.push(event.data);
+												else
+												{
+													if (isCapturing)
+													{
+														var data = concatenation(chank);
+
+														decodeBuffer(data);
+
+														for (var key in dataChannel_offer)
+															sendData(dataChannel_offer[key], chank);
+													}
+
+													chank = [];
+												}
+											};
+										};
+
+										var data = 
+										{
+											"type" : MessageType.SDPAnswer, 
+											"id": peerID, 
+											"targetId": message.id, 
+											"data": sdp,
+										};
+										var json = JSON.stringify(data);
+										socket.Send(json);
+
+										debugLog("send SDPAnswer to id: " + message.id);
+									},
+									function() { debugLog("setLocalDescription failed."); });
+							},
+							function() { debugLog("createAnswer failed."); });
+					},
+					function() { debugLog("setRemoteDescription failed."); });
+				break;
+
+				case MessageType.SDPAnswer: // SDP Answer Received
+				debugLog("received answer from id: " + message.id);
+				peer_offer[message.id].setRemoteDescription(
+					new webRTCSessionDescription(message.data),
+					function() { debugLog("setRemoteDescription succeeded."); },
+					function() { debugLog("setRemoteDescription failed."); });
+				break;
+
+				case MessageType.ICECandidateOffer: // ICECandidate Received from Offer
+				debugLog("received ICECandidate from offer id: " + message.id);
+				peer_answer.addIceCandidate(new webRTCIceCandidate(message.data));
+				break;
+
+				case MessageType.ICECandidateAnswer: // ICECandidate Received from Answer
+				debugLog("received ICECandidate from answer id: " + message.id);
+				peer_offer[message.id].addIceCandidate(new webRTCIceCandidate(message.data));
+				break;
+
+				case MessageType.RemoveOffer: // Remove Offer
+				debugLog("remove offer id: " + message.id);
+				peer_offer[message.id].close();
+				peer_offer[message.id] = null;
+				break;
+
+				case MessageType.RemoveAnswer: // Remove Answer
+				debugLog("remove answer.");
+				peer_answer.close();
+				peer_answer = null;
+				break;
+
+				case MessageType.Settings:
+				var dn = divisionNum;
+
+				audioSampleRate = message.data.audioSettingData.sampleRate;
+				audioIsStereo = message.data.audioSettingData.isStereo ? 2 : 1;
+
+				divisionNum = message.data.captureSettingData.divisionNum;
+
+				while (block.firstChild)
+					block.removeChild(block.firstChild);
+
+				for (var row = 0; row < divisionNum; row++)
+				{
+					var rowElem = document.createElement("div");
+					block.appendChild(rowElem);
+				
+					for (var cell = 0; cell < divisionNum; cell++)
+					{
+						var cellElem = document.createElement('div');
+						var base = document.createElement('img');
+						var cover = document.createElement('img');
+						cover.className = "cover";
+						cellElem.appendChild(base);
+						cellElem.appendChild(cover);
+						rowElem.appendChild(cellElem);
+					}
+				}
+
+				loadingQueue = Array.apply(null, Array(divisionNum * divisionNum)).map(function () { return 0 });
+
+				settings = clone(message.data);
+				break;
+
+				case MessageType.StartCapture:
+				isCapturing = true;
+				break;
+
+				case MessageType.StopCapture:
+				isCapturing = false;
+				break;
+
+				case MessageType.ReceiveTimeout:
+				break;
+
+				case MessageType.Disconnect:
+				socket.Stop();
+				break;
+			}
+		}
+		else if (isCapturing)
+		{
+			decodeBuffer(event.data);
+
+			var chank = segmentation(event.data, ChankSize);
+
+			for (var key in dataChannel_offer)
+				sendData(dataChannel_offer[key], chank);
+		}
+
+		function decodeBuffer(data)
+		{
+			var arr = new Uint8Array(data);
+			var type = arr[0];
+
+			switch (type)
+			{
+				case DataType.FrameBuffer:
+				var segIdx = arr[1];
+
+				if (loadingQueue[segIdx] > 1)
+					break;
+
+				var dataArr = new Uint8Array(data, FrameHeaderLength);
+				var blob = new Blob([dataArr], {type:'image/jpeg'});
+				var url = urlCreator.createObjectURL(blob);
+
+				var segX = segIdx % divisionNum;
+				var segY = Math.floor(segIdx / divisionNum);
+
+				var elem = block.children[segY].children[segX];
+
+				elem.children[1].onload = function()
+				{
+					elem.insertBefore(elem.children[1], elem.children[0]);
+
+					elem.children[0].className = "";
+					elem.children[1].className = "cover";
+					
+					if (elem.children[1] != null)
+						urlCreator.revokeObjectURL(elem.children[1].src);
+
+					loadingQueue[segIdx]--;
+				}
+
+				elem.children[1].src = url;
+				loadingQueue[segIdx]++;
+
+				latestSegmentData[segIdx] = data.slice(0);
+				break;
+
+				case DataType.AudioBuffer:
+				if (!audioCheckBox.checked) 
+					break;
+
+				var audioArr = new Float32Array(data, AudioHeaderLength);
+				var audioBuf = audioCtx.createBuffer(audioIsStereo, audioArr.length, audioSampleRate);
+				var audioSrc = audioCtx.createBufferSource();
+
+				var audioBufArr = audioBuf.getChannelData(0);
+				audioBufArr.set(audioArr);
+
+				audioSrc.buffer = audioBuf;
+				audioSrc.connect(audioCtx.destination);
+
+				if (audioCtx.currentTime < audioTime)
+				{
+					audioSrc.start(audioTime);
+					audioTime += audioBuf.duration;
+				}
+				else
+				{
+					audioSrc.start(audioTime);
+					audioTime = audioCtx.currentTime + audioBuf.duration;
+				}
+
+				break;
+			}
+		}
+
+		function sendData(offer, chank)
+		{
+			if(offer.readyState != 'open') 
+				return;
+
+			for (var i = 0; i < chank.length; i++)
+				offer.send(chank[i]);
+
+			offer.send("\0");
+		}
+
+		function segmentation(arrayBuffer, segmentSize)
+		{
+			var segments= [];
+
+			for(var i = 0; i * segmentSize < arrayBuffer.byteLength; i++)
+				segments.push(arrayBuffer.slice(i * segmentSize, (i + 1) * segmentSize));
+
+			return segments;
+		}
+
+		function concatenation(segments)
+		{
+			var sumLength = 0;
+
+			for(var i = 0; i < segments.length; i++)
+				sumLength += segments[i].byteLength;
+
+			var whole = new Uint8Array(sumLength);
+			var pos = 0;
+
+			for(var i = 0; i < segments.length; i++)
+			{
+				whole.set(new Uint8Array(segments[i]), pos);
+				pos += segments[i].byteLength;
+			}
+
+			return whole.buffer;
+		}
+
+		function clone(src)
+		{
+			var dst = {}
+			
+			for(var k in src)
+				dst[k] = src[k];
+
+			return dst;
+		}
+	};
+
+	socket.Start();
+
+	function debugLog(log)
+	{
+		if (__DEBUG__)
+			console.log(log);
+	}
+}
+
+
+
+
