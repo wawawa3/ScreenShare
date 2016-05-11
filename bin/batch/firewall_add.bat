630a6763b7cd810eb15d4a0dfca0cf53df11a1c8
Wawawa <id.253122@gmail.com>
2016-05-11 15:23:11 +0900

    修正

--- bin/batch/firewall_add.bat
+++ bin/batch/firewall_add.bat
@@ -1,7 +1,6 @@
 @echo off
 
-netsh advfirewall firewall add rule name="HttpListener" dir=in action=allow
-netsh advfirewall firewall set rule name="HttpListener" new program="system" profile=private protocol=tcp localport=8080
+netsh advfirewall firewall add rule name="HttpListener" dir=in action=allow program="system" profile=private,public localport=8080 protocol=tcp
 netsh http add urlacl url="http://+:8080/" user=everyone
 
 timeout 1
--- bin/batch/firewall_delete.bat
+++ bin/batch/firewall_delete.bat
@@ -1,6 +1,6 @@
 @echo off
 
-netsh advfirewall firewall delete rule name="HttpListener" program="system"
+netsh advfirewall firewall delete rule name="HttpListener"
 netsh http delete urlacl url="http://+:8080/"
 
 timeout 1
